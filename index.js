a8aa6665950fb2ee78913ce6045f6a9d7cda026d
L-bot532 <mwauralevite@gmail.com>
2026-01-04 13:13:01 +0300

    Change axios request from adams to levie

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const adams = require("./config");
 
 async function fetchINDEXUrl() {
   try {
-    const response = await axios.get(adams.BWM_XMD);
+    const response = await axios.get(levie.BWM_XMD);
     const $ = cheerio.load(response.data);
 
     const targetElement = $('a:contains("INDEX")');

9c2c3850d54ff0293293519a3fb6f2604e15946b
L-bot532 <mwauralevite@gmail.com>
2026-01-04 12:56:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //  [BWM-XMD QUANTUM EDITION]                                           
 //  >> A superposition of elegant code states                           
 //  >> Collapsed into optimal execution                                
-//  >> Scripted by Sir Ibrahim Adams                                    
+//  >> Scripted by levie Njenga                                  
 //  >> Version: 8.3.5-quantum.7
 
 const axios = require('axios');
@@ -11,7 +11,7 @@ const adams = require("./config");
 
 async function fetchINDEXUrl() {
   try {
-    const response = await axios.get(adams.BWM_XMD);
+    const response = await axios.get(levie.BWM_XMD);
     const $ = cheerio.load(response.data);
 
     const targetElement = $('a:contains("INDEX")');
